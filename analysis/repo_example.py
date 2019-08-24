5b5fc558fd172b243852f2dd7e424c2e6860cdfd
benjyz <ben@enet.io>
2019-08-24 11:34:16 -0700

    search example

--- analysis/repo_example.py
+++ analysis/repo_example.py
@@ -1,3 +1,7 @@
+"""
+basic example to analyse the repos
+"""
+
 import toml
 import os
 
@@ -5,7 +9,7 @@ def get_info():
     """ get all information from github repos """
     datadir = "data/ecosystems"
     dd = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', datadir))
-    print (dd)
+    print ("analysing ",dd)
     orgs = list()
     repos = list()
     for dirpath, dnames, fnames in os.walk(dd):
@@ -17,6 +21,17 @@ def get_info():
                     repos.extend(content["repo"])
     return [orgs,repos]
 
+def basic_example(repos, name):
+    c = 0
+    for r in repos:    
+        if name in r["url"]:
+            #print (r)
+            c+=1
+
+    print ("repos with %s in its name %i"%(name,c))
+
 [orgs,repos] = get_info()
 print ("orgs ", len(orgs))
 print ("repos ", len(repos))
+basic_example(repos, "bitcoin")
+basic_example(repos, "trading")

129b0d948c9b9afa5de3b49e0810830d0135f7fd
MrRobotCrypto <hsn.dyn721@gmail.com>
2025-10-14 17:22:20 +0300

    Anoma: add public docs/spec repositories (round 2) (#2295)
    Signed-off-by: mrrobotcrypto <hsn.dyn721@gmail.com>

---
+++ migrations/2025-10-13T173000_add_anoma_docs_round2.sql
@@ -0,0 +1,8 @@
+-- 2025-10-13T17:30:00Z add_anoma_docs_round2
+-- Public documentation/spec repositories under the official `anoma` org
+
+repadd Anoma https://github.com/anoma/nspec                   #spec #docs
+repadd Anoma https://github.com/anoma/whitepaper              #whitepaper #docs
+repadd Anoma https://github.com/anoma/specs.anoma.net         #docs #website
+repadd Anoma https://github.com/anoma/developer-docs          #docs #devrel
+repadd Anoma https://github.com/anoma/anoma-beta-documentation #docs #beta
